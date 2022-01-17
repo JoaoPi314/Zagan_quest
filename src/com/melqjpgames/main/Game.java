6951d29dbd33865e86fde0dcd0a67283f030de82
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2022-01-17 02:02:54 -0300

    Creates baseline

---
+++ src/com/melqjpgames/main/Game.java
@@ -0,0 +1,134 @@
+package com.melqjpgames.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 4;
+	
+	private BufferedImage image;
+
+	
+	public Game() {
+
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initFrame();
+		// Everything is scalable, to undo this, multiply here by scale
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		
+		
+	}
+	
+	public void initFrame() {
+		frame = new JFrame("Game #1");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+	
+
+	
+	public void update() {
+
+	}
+
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+		//
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image,  0,  0,  WIDTH*SCALE,  HEIGHT*SCALE, null);
+		bs.show();
+		
+	}
+	
+	@Override
+	public void run() {
+		// Catch current time from PC
+		long lastTime = System.nanoTime();
+		// FPS
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0.0;
+		
+		// Check FPS
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		
+		
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				update();
+				render();
+				delta--;
+				frames++;
+			}
+			
+			// Check FPS
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
+		
+		stop();
+		
+	}
+
+}
