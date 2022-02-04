ffc23366f9c5697887da22cba3c3feb8fbbd87c5
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2022-02-04 02:21:01 -0300

    Refactors Spritesheet class

--- src/com/wellmax/graphics/Spritesheet.java
+++ src/com/wellmax/graphics/Spritesheet.java
@@ -5,26 +5,40 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * The spritesheet is a class that will load a spritesheet 
+ * @author joao.gomes
+ *
+ */
 public class Spritesheet {
 	
-	
+	/**
+	 * Spritesheet to be loaded
+	 */
 	private BufferedImage spritesheet;
 	
 	
-	/*
-	* The Constructor of Spritesheet receives a path to spritesheet file
-	*/
+
+	/**
+	 * Constructor
+	 * @param path Path to spritesheet image
+	 */
 	public Spritesheet(String path) {	
 		try {
 			spritesheet = ImageIO.read(getClass().getResource(path));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
 	
-	/*
-	 * The getSprite returns the current sprite from spritesheet
+
+	/**
+	 * Method to return the current sprite from spritesheet
+	 * @param x Sprite x position
+	 * @param y sprite y position
+	 * @param width Sprite width
+	 * @param height Sprite height
+	 * @return Sprite in BufferedImage format
 	 */
 	public BufferedImage getSprite(int x, int y, int width, int height) {
 		return spritesheet.getSubimage(x, y, width, height);
