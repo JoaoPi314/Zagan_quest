2f84b917ca7409b7518324f343a9b76b14dbbf88
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2022-02-04 00:22:31 -0300

    Refactors FloorTile class

--- src/com/wellmax/world/FloorTile.java
+++ src/com/wellmax/world/FloorTile.java
@@ -2,8 +2,19 @@ package com.wellmax.world;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * FloorTile doesn't have collision with entities
+ * @author joao.gomes
+ *
+ */
 public class FloorTile extends Tile{
 
+	/**
+	 * FloorTile constructor. It only calls super constructor
+	 * @param x x position
+	 * @param y y position
+	 * @param sprite Tile sprite
+	 */
 	public FloorTile(int x, int y, BufferedImage sprite) {
 		super(x, y, sprite);
 	}
