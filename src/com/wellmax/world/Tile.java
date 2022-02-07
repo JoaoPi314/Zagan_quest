e276fb8c3037da51622c307965c8f45c72a44e53
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2022-02-07 15:39:32 -0300

    Removes getter and setter comments and static initializer comment

--- src/com/wellmax/world/Tile.java
+++ src/com/wellmax/world/Tile.java
@@ -66,9 +66,6 @@ public class Tile {
 	 */
 	public static BufferedImage TILE_STATUE;
 	
-	/**
-	 * Static initializations
-	 */
 	static {
 		// Arrays
 		TILE_GRASS = new BufferedImage[3][3];
@@ -108,58 +105,35 @@ public class Tile {
 		this.setHeight(16);
 	}
 
-	/**
-	 * @return the x
-	 */
+
 	public int getX() {
 		return x;
 	}
 
-	/**
-	 * @param x the x to set
-	 */
 	public void setX(int x) {
 		this.x = x;
 	}
-	
-	/**
-	 * @return the y
-	 */
+
 	public int getY() {
 		return y;
 	}
-	
-	/**
-	 * @param y the y to set
-	 */
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
-	/**
-	 * @return the width
-	 */
+
 	public int getWidth() {
 		return width;
 	}
 
-	/**
-	 * @param width the width to set
-	 */
 	public void setWidth(int width) {
 		this.width = width;
 	}
 
-	/**
-	 * @return the height
-	 */
 	public int getHeight() {
 		return height;
 	}
 
-	/**
-	 * @param height the height to set
-	 */
 	public void setHeight(int height) {
 		this.height = height;
 	}
